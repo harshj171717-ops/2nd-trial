338f7921f0b65dab733cc76f0939e109d2985efa
harshj171717-ops <harshj171717@gmail.com>
2026-09-24 11:49:04 +0530

    Stacks file added!

---
+++ src/Stacks.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Stacks {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.pop();
+        System.out.println(st.peek());
+        System.out.println(st);
+    }
+}
